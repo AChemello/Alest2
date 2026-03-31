15720abe22d6090dca996bd3881a3ed91f0d22c6
AChemello <arthur.chemello@edu.pucrs.br>
2026-03-31 08:57:24 -0300

    Aula1e2maisHash

---
+++ appheap.cpp
@@ -0,0 +1,133 @@
+// MaxHeap.java
+public class MaxHeap<T extends Comparable<T>> {
+
+  private T v[];
+  private int size;
+
+  @SuppressWarnings("unchecked")
+  public MaxHeap(int tam) {
+    size = 1;
+    v = (T[]) new Comparable[tam];
+  }
+
+  public MaxHeap(T values[]) {
+    v = values;
+    size = values.length;
+  }
+
+  private void swim(int k) {
+    while (k > 1 && v[k / 2].compareTo(v[k]) < 0) {
+      System.out.println("Swap " + v[k] + " with " + v[k / 2] + " " + k);
+      T tmp = v[k];
+      v[k] = v[k / 2];
+      v[k / 2] = tmp;
+      k = k / 2;
+    }
+  }
+
+  public void put(T data) {
+    v[size] = data;
+    swim(size);
+    size++;
+  }
+
+  private void sink(int k, int n) {
+    while (2 * k <= n) {
+      int j = 2 * k;
+      if (j < n && v[j].compareTo(v[j + 1]) < 0)
+        j++;
+      if (v[k].compareTo(v[j])>=0)
+        break;
+      T tmp = v[k];
+      v[k] = v[j];
+      v[j] = tmp;
+      k = j;
+    }
+  }
+
+  public T get() {
+    if (size == 1)
+      throw new UnsupportedOperationException("MaxHeap vazio!");
+    T res = v[1];
+    v[1] = v[--size];
+    sink(1,size);
+    return res;
+  }
+
+  private void print(int b, int elem, int sp) {
+    int i, j;
+
+    System.out.println("");
+    for (j = 0; j < size; j++)
+      System.out.print(v[j] + " ");
+    System.out.println("");
+
+    while (true) {
+      for (j = 0; j <= sp / 2; j++)
+        System.out.print(" ");
+      for (i = b; i < b + elem; i++) {
+        if (i == size)
+          return;
+        System.out.print(v[i]);
+        for (j = 0; j < sp; j++)
+          System.out.print(" ");
+      }
+      System.out.println("");
+      b = b + elem;
+      elem = 2 * elem;
+      sp = sp / 2;
+    }
+  }
+
+  public void print() {
+    System.out.println("");
+    print(1, 1, 32);
+    System.out.println("");
+  }
+
+  public void printArray() {
+    for (int i = 0; i < size; i++)
+      System.out.print(v[i] + " ");
+    System.out.println();
+  }
+
+  // Implemente heapsort!
+  void sort(T[] data) {
+    // Aponta v para data, pois queremos alterar
+    // o vetor ORIGINAL lá fora
+    v = data;
+    size = v.length;
+    int n = size-1;
+
+    // print();
+
+    // 1. Transforma o vetor de entrada em um maxheap,
+    // usando a técnica bottom-up (de baixo para cima)
+
+    for(int k=n/2; k>=1; k--) {
+      sink(k, n);
+    }
+
+    // 2. Ordena no próprio vetor, fazendo um "delMax"
+    // SEM remover o maior elemento
+
+    while(n>1) {
+      T tmp = v[1];
+      v[1] = v[n];
+      v[n] = tmp;
+      n--;
+      sink(1,n);
+    }
+
+    // print();
+  }
+
+  public static void main(String[] args) {
+    MaxHeap<Integer> mHeap = new MaxHeap<>(100);
+    Integer[] vet = { -1, 17, 8, 1, 4, 100, 45, 23, 18, 28, 30};
+    mHeap.sort(vet);
+    for(int v: vet)
+        System.out.print(v+" ");
+    System.out.println();
+  }
+}
